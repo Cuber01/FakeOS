a1d2b5ed6b95e4225476ca1727a98d9539879ab2
Cuber01 <johnmallard631@gmail.com>
2022-02-19 14:41:28 +0100

    Added a dummy menubar

--- FakeOS/Software/TextEditor.cs
+++ FakeOS/Software/TextEditor.cs
@@ -6,12 +6,28 @@ namespace FakeOS.Software;
 
 public class TextEditor : GuiSoftware
 {
+    /*  menu at the top of the screen
+    
+        if(ImGui.BeginMainMenuBar())
+        {
+            if (ImGui.BeginMenu("File"))
+            {
+                if(ImGui.MenuItem("New"))
+                {
+                    //Do something
+                }
+                ImGui.EndMenu();
+            }
+        
+            ImGui.EndMainMenuBar();
+        }
+    
+    */
+    
     private string text = "";
     private readonly ImGuiInputTextFlags multilineTextFlags = ImGuiInputTextFlags.CallbackCharFilter | ImGuiInputTextFlags.AllowTabInput;
     private readonly ImGuiWindowFlags windowFlags = ImGuiWindowFlags.Modal | ImGuiWindowFlags.MenuBar;
 
-    private bool pushedColor = false;
-    
     public TextEditor()
     {
         this.name = "Text Editor";
@@ -24,14 +40,79 @@ public class TextEditor : GuiSoftware
         ImGui.Begin(name, ref running, windowFlags);
 
         ImGui.InputTextMultiline("", ref text, UInt16.MaxValue, new Vector2(1000, 1000),
-            multilineTextFlags, processData);
-        
+            multilineTextFlags);
+
+        menuBar();
+
         ImGui.End();
     }
 
-    private unsafe int processData(ImGuiInputTextCallbackData* data)
+    private void menuBar()
     {
-        return 0;
+        ImGui.BeginMenuBar();
+        
+        if (ImGui.BeginMenu("Menu"))
+        {
+            if(ImGui.MenuItem("New"))
+            {
+                //Do something
+            }
+            
+            if(ImGui.MenuItem("Open", "Ctrl + O"))
+            {
+                //Do something
+            }
+            
+            ImGui.Separator();
+            
+            if(ImGui.MenuItem("Save", "Ctrl + S"))
+            {
+                //Do something
+            }
+            
+            if(ImGui.MenuItem("Save as"))
+            {
+                //Do something
+            }
+
+            ImGui.Separator();
+            
+            if(ImGui.MenuItem("Quit", "Alt + F4"))
+            {
+                //Do something
+            }
+            
+            ImGui.EndMenu();
+        }
+
+        
+        ImGui.EndMenuBar();
+        
+        //
+        // ImGui.MenuItem("(demo menu)", null, false, false);
+        // if (ImGui.MenuItem("New")) {}
+        // if (ImGui.MenuItem("Open", "Ctrl+O")) {}
+        // if (ImGui.BeginMenu("Open Recent"))
+        // {
+        //     ImGui.MenuItem("fish_hat.c");
+        //     ImGui.MenuItem("fish_hat.inl");
+        //     ImGui.MenuItem("fish_hat.h");
+        //     if (ImGui.BeginMenu("More.."))
+        //     {
+        //         ImGui.MenuItem("Hello");
+        //         ImGui.MenuItem("Sailor");
+        //         if (ImGui.BeginMenu("Recurse.."))
+        //         {
+        //             ImGui.EndMenu();
+        //         }
+        //         ImGui.EndMenu();
+        //     }
+        //     ImGui.EndMenu();
+        // }
+        // if (ImGui.MenuItem("Save", "Ctrl+S")) {}
+        // if (ImGui.MenuItem("Save As..")) {}
+
     }
+    
     
 }
