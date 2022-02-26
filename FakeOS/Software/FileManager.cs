83be6f8e7e2b0357aa56db2dcec19979ac8864dd
Cuber01 <johnmallard631@gmail.com>
2022-02-26 11:48:45 +0100

    Added getting file info

--- FakeOS/Software/FileManager.cs
+++ FakeOS/Software/FileManager.cs
@@ -10,17 +10,22 @@ namespace FakeOS.Software;
 
 public class FileManager : GuiSoftware
 {
+    private Dictionary<string, string> filesAndTypes = new Dictionary<string, string>();
+    private readonly string pathToFilesystem;
+
+    private const int framesBetweenFileChecks = 60;
+    private int fileCheckTimer = 0;
+    
     private const ImGuiWindowFlags windowFlags = ImGuiWindowFlags.Modal | ImGuiWindowFlags.MenuBar;
     private const ImGuiTableFlags tableFlags = ImGuiTableFlags.BordersOuter | ImGuiTableFlags.Resizable;
 
     private const ImGuiTreeNodeFlags folderFlags = ImGuiTreeNodeFlags.Leaf | ImGuiTreeNodeFlags.NoTreePushOnOpen | ImGuiTreeNodeFlags.Bullet;
     private const ImGuiTreeNodeFlags fileFlags = ImGuiTreeNodeFlags.Leaf | ImGuiTreeNodeFlags.NoTreePushOnOpen;
 
-    public FileManager()
+    public FileManager(string pathToFilesystem)
     {
         name = "File Manager";
-        
-        showFilesystem("/home/cubeq/");
+        this.pathToFilesystem = pathToFilesystem;
     }
 
     public override void draw()
@@ -37,17 +42,35 @@ public class FileManager : GuiSoftware
         
         ImGui.End();
     }
+    
+    public override void update()
+    {
+        if (!running) return;
 
+        fileCheckTimer++;
+        if (fileCheckTimer == framesBetweenFileChecks)
+        {
+            fileCheckTimer = 0;
+            getFilesAndTypes();
+        }
+    }
+
+    private void getFilesAndTypes()
+    {
+        string[] files = Directory.GetFiles(pathToFilesystem);
+
+        for(int i = 0; i <= files.Length; i++)
+        {
+            filesAndTypes.Add(files[i], MimeTypes.GetContentType(files[i]));
+        }
+    }
+    
     private void showTable()
     {
 
-        if (ImGui.BeginTable("split", 2, tableFlags))
+        if (ImGui.BeginTable("split", 1, tableFlags))
         {
-
-            for (int obj_i = 0; obj_i < 4; obj_i++)
-            {
-                showFile("Object", obj_i);
-            }
+            showFile("Filesystem", 1);
             
             ImGui.EndTable();
         }
@@ -57,14 +80,22 @@ public class FileManager : GuiSoftware
     {
         string[] files = Directory.GetFiles(path);
 
-        foreach (var file in files)
+        for(int i = 0; i <= files.Length; i++)
         {
-            Console.WriteLine(MimeTypes.GetContentType(file));
+            showFile(files[i], i);
         }
     }
 
-    private void showFile(string filename, int id)
+    private void showFile(string file, int id)
     {
+        // if (MimeTypes.GetContentType(file) == "application/octet-stream")
+        // {
+        //     string[] otherFiles = Directory.GetFiles(file);
+        //
+        //     if (otherFiles.Length == 0) continue;
+        // }
+        //
+        
         // Use object uid as identifier. Most commonly you could also use the object pointer as a base ID.
         ImGui.PushID(id);
         
@@ -73,7 +104,7 @@ public class FileManager : GuiSoftware
         ImGui.TableSetColumnIndex(0);
         ImGui.AlignTextToFramePadding();
         
-        bool nodeOpen = ImGui.TreeNode("#file" + id, filename);
+        bool nodeOpen = ImGui.TreeNode("#file" + id, file);
 
         if (nodeOpen)
         {
@@ -103,9 +134,5 @@ public class FileManager : GuiSoftware
         
         ImGui.PopID();
     }
-
-    public override void update()
-    {
-        base.update();
-    }
+    
 }
